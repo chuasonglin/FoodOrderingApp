a9a1a08c2d42fd22ad28d6a8e24414ebc16ae782
Song Lin <chua0812@e.ntu.edu.sg>
2021-06-20 23:55:01 +0800

    basic redux completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import { compiler, pipe } from 'lodash/fp'
+import { bugAdded, bugRemoved, bugResolved } from "./actions";
 
 import store from "./store";
 
-store.subscribe(() => {
-    cons
+const unsubscribe = store.subscribe(() => {
+    console.log("Store changed", store.getState())
 })
+
+store.dispatch(bugAdded("Bug 1"))
+store.dispatch(bugResolved(1))
+
+unsubscribe()
+
+console.log(store.getState())
